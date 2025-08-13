0ed7b8691aa1748c43fca0a2aa1c89978e1127a2
armyv7v <86036225+armyv7v@users.noreply.github.com>
2025-08-13 16:45:42 -0400

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,21 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
-import tagger from "@dhiwise/component-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
+  plugins: [react()],
+  base: './', // Esto ayuda con las rutas relativas
   build: {
-    outDir: "build",
-    chunkSizeWarningLimit: 2000,
-  },
-  plugins: [tsconfigPaths(), react(), tagger()],
-  server: {
-    port: "4028",
-    host: "0.0.0.0",
-    strictPort: true,
-    allowedHosts: ['.amazonaws.com', '.builtwithrocket.new']
+    outDir: 'dist',
+    emptyOutDir: true
   }
-});
+})
